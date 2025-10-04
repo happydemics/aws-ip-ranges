bcfa46cf33f29bc7bd6ab97d875459b279657b10
Thomas FONTAINE <thomas.fontaine@me.com>
2025-10-04 10:06:16 -0400

    Update ruby & gems

--- Gemfile
+++ Gemfile
@@ -6,10 +6,12 @@ source "https://rubygems.org"
 gemspec
 
 gem "minitest", "~> 5.0"
-gem "rack", "~> 2.2"
+gem "rack", "~> 3"
+gem "rackup", "~> 2.2"
+
 gem "rake", "~> 13.0"
 gem "rubocop", "~> 1.7"
-gem "sinatra", "~> 2.2"
-gem "webrick", "~> 1.7"
+gem "sinatra", "~> 4"
+gem "webrick", "~> 1.9"
 
 gem "pry-byebug", "~> 3.9"
--- test/aws_ip_ranges_test.rb
+++ test/aws_ip_ranges_test.rb
@@ -3,23 +3,41 @@
 require "test_helper"
 
 require "rack"
+require "rackup"          # Rack 3: handlers live here
+require "rack/files"      # Rack::Files (replacement for old Rack::File)
 require "webrick"
 require "ipaddr"
 
 class AwsIpRangesTest < Minitest::Test
   def setup
     logger = WEBrick::Log.new(nil, WEBrick::BasicLog::WARN)
-    @server = WEBrick::HTTPServer.new(Host: @host = "localhost", Port: @port = 22_222, Logger: logger, AccessLog: [])
-    @server.mount "/", Rack::Handler::WEBrick, Rack::File.new("test/fixtures")
 
-    @thread = Thread.new { @server.start }
-    trap(:INT) { @server.shutdown }
+    @host = "127.0.0.1"
+    @server = nil
+
+    # Serve the fixtures directory as a Rack app
+    app = Rack::Files.new("test/fixtures")
+
+    # Start WEBrick via Rackup on a free port (Port: 0)
+    @thread = Thread.new do
+      Rackup::Handler.get(:webrick).run(
+        app,
+        Host: @host,
+        Port: 0,
+        Logger: logger,
+        AccessLog: []
+      ) do |server|
+        @server = server
+      end
+    end
+
     wait_for_server_ready
+    @port = @server.config[:Port]
   end
 
   def teardown
-    @server.shutdown
-    @thread.join
+    @server&.shutdown
+    @thread&.join
   end
 
   def test_it_fetches_all_ips
@@ -49,8 +67,8 @@ class AwsIpRangesTest < Minitest::Test
   def test_it_failed_to_fetch
     @server.shutdown
 
-    expected_msg = "Failed to load IP ranges: Failed to open TCP connection to localhost:22222 "\
-                   "(Connection refused - connect(2) for 127.0.0.1:22222)"
+    expected_msg = "Failed to load IP ranges: Failed to open TCP connection to #{@host}:#{@port} " \
+                   "(Connection refused - connect(2) for 127.0.0.1:#{@port})"
     assert_raises(Faraday::ConnectionFailed, expected_msg) { fetch }
   end
 
@@ -83,7 +101,7 @@ class AwsIpRangesTest < Minitest::Test
     ips, err = cli
 
     assert_equal([], ips)
-    assert_match(/Failed to load IP ranges: Failed to open TCP connection to localhost:22222/, err)
+    assert_match(/Failed to load IP ranges: Failed to open TCP connection to #{@host}:#{@port}/, err)
   end
 
   private
@@ -95,7 +113,7 @@ class AwsIpRangesTest < Minitest::Test
   end
 
   def assert_match_fixture(ips, fixture_name)
-    content =    File.readlines(File.join("test/fixtures", "#{fixture_name}.csv"), chomp: true)
+    content = File.readlines(File.join("test/fixtures", "#{fixture_name}.csv"), chomp: true)
     expected_ips = content.map { |ip| IPAddr.new(ip) }
     assert_equal(sort_ips(expected_ips), sort_ips(ips))
   end
@@ -109,7 +127,7 @@ class AwsIpRangesTest < Minitest::Test
   end
 
   def fetch(**kwargs)
-    AwsIpRanges.fetch(base_url: "http://localhost:22222/", **kwargs)
+    AwsIpRanges.fetch(base_url: "http://#{@host}:#{@port}/", **kwargs)
   end
 
   def cli(only_ipv4: nil, only_ipv6: nil, region: nil, service: nil)
@@ -120,21 +138,27 @@ class AwsIpRangesTest < Minitest::Test
       opts = "#{opts} --region #{region.join(",")}" if region
       opts = "#{opts} --service #{service.join(",")}" if service
 
-      system "AWS_IP_RANGE_HOST_URL='http://localhost:22222' ruby -Ilib exe/aws-ip-ranges list #{opts}"
+      system "AWS_IP_RANGE_HOST_URL='http://#{@host}:#{@port}' ruby -Ilib exe/aws-ip-ranges list #{opts}"
     end
     [out.split("\n"), err]
   end
 
   def wait_for_server_ready
-    seconds = 10
-    wait_time = 0.01
-    until @server.status == :Running || seconds <= 0
-      seconds -= wait_time
-      sleep wait_time
+    # Wait until the handler yields a server instance
+    seconds = 10.0
+    wait = 0.01
+    until @server || seconds <= 0
+      seconds -= wait
+      sleep wait
     end
+    raise "Server was not created" unless @server
 
-    return if @server.status == :Running
-
-    raise "Server was not started"
+    # Then wait for WEBrick to be running
+    seconds = 10.0
+    until @server.status == :Running || seconds <= 0
+      seconds -= wait
+      sleep wait
+    end
+    raise "Server was not started" unless @server.status == :Running
   end
 end
